e7ed4154aeda795aa4952226e81b15f867659d64
mono245 <matvejotinov804@gmail.com>
2024-02-22 02:30:44 +0500

    logging method changed

--- WeatherBot/bot.py
+++ WeatherBot/bot.py
@@ -1,7 +1,7 @@
-import logging
 import asyncio
 from os import getenv
 from dotenv import load_dotenv
+from loguru import logger
 
 from aiohttp import ClientSession
 
@@ -12,19 +12,20 @@ from aiogram.enums.parse_mode import ParseMode
 from aiogram.utils.formatting import Bold, Text
 
 load_dotenv()
-logging.basicConfig(level=logging.INFO)  # enable logging
 
 bot = Bot(getenv("BOT_TOKEN"), parse_mode=ParseMode.HTML)
 dp = Dispatcher()
 
 
 async def get_json_weather(city_name: str) -> dict | list:
+    url = "https://api.openweathermap.org/data/2.5/weather?" +\
+        f"q={city_name}&appid={getenv('WEATHER_TOKEN')}&units=metric&lang=ru"
+
     async with ClientSession() as session:
-        async with session.get(
-            "https://api.openweathermap.org/data/2.5/weather?" +
-            f"q={city_name}&appid={getenv('WEATHER_TOKEN')}&units=metric&lang=ru"
-        ) as response:
+        async with session.get(url) as response:
             json_data = await response.json()
+
+            logger.info(f"Request to \"{city_name}\": {json_data['cod']}")
 
             return json_data
 
@@ -34,19 +35,36 @@ async def start(message: types.Message) -> None:
     content = Text(
         "Привет, ", Bold(message.from_user.full_name), ". Введи название города, чтобы узнать там погоду"
     )
+
+    logger.info(f"/start handled from @{message.from_user.username}")
     
     await message.answer(**content.as_kwargs())
 
 
+@dp.message(Command("src"))
+async def src(message: types.Message) -> None:
+    logger.info(f"/src handled from @{message.from_user.username}")
+
+    await message.reply(
+        "Исходный код <a href=\"https://github.com/mono245/WeatherBot\">здесь</a>",
+        disable_web_page_preview=True
+    )
+
+
 @dp.message(F.text)
 async def cities_handler(message: types.Message) -> None:
-    json_weather = await get_json_weather(message.text)
+    json_weather = await get_json_weather(message.text.strip())
+    username = message.from_user.username
 
     if int(json_weather['cod']) > 226:  # bad request
+        logger.warning(f"Bad request handled from @{username}")
+
         await message.reply(
             f"Что-то пошло не так, \n<b>{json_weather['cod']}: {json_weather['message']}</b>"
         )
     else:  # ok request
+        logger.info(f"OK request handled from @{username}, city: {message.text}")
+
         await message.reply(
             f"<b>{json_weather['main']['temp']:.0f}С°</b>, "
             f"<b>{json_weather['weather'][0]['description']}</b>, "
